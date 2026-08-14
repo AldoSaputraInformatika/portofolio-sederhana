0dc61b2883d288f129c058cc148a4d0a098c9f13
AldoSaputraInformatika <aldopadaungan@gmail.com>
2026-08-14 18:50:03 +0800

    uptodate

--- hhhhhh.js
+++ hhhhhh.js
@@ -1,6 +1,8 @@
 let btn = document.getElementById("tglbtn");
 let sidebar = document.getElementById("sidebar");
 
+
 btn.addEventListener("click", function () {
     sidebar.classList.toggle("hidden2")
+    btn.classList.toggle("cdss")
 })
