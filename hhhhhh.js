d1982f21709e8677029bcab96ccd7354b3eb5b3e
AldoSaputraInformatika <aldopadaungan@gmail.com>
2026-08-14 18:26:33 +0800

    uptodate

--- hhhhhh.js
+++ hhhhhh.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector('.container .top .link .hamburger input');
-const navnya = document.querySelector('.container .top .link ul');
+let btn = document.getElementById("tglbtn");
+let sidebar = document.getElementById("sidebar");
 
-hamburger.addEventListener('click', function (){
-    navnya.classList.toggle('slide')
+btn.addEventListener("click", function () {
+    sidebar.classList.toggle("hidden2")
 })
